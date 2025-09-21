3b4fea01e1d21ac228e59a92c3e1080b272b411c
Isak Friis-Jespersen <isak.friisjespersen2@gmail.com>
2025-09-21 16:35:58 +0200

    fixed auto session

--- nvim/lua/ifj/plugins/auto-session.lua
+++ nvim/lua/ifj/plugins/auto-session.lua
@@ -1,16 +1,18 @@
 return {
   "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { },
+    auto_restore_last_session = true,
+  },
   config = function()
-    local auto_session = require("auto-session")
-
-    auto_session.setup({
-      auto_restore_enabled = true,
-      auto_session_suppress_dirs = { "~/", "~/booli/", "~/coding", "~/config" },
-    })
-
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    keymap.set("n", "<leader>wr", "<cmd>AutoSession restore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+    keymap.set("n", "<leader>ws", "<cmd>SessionRestore<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
   end,
 }
