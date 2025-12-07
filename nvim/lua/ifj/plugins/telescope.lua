21972a77347a0cd6fa34ee0862122a3d97f4c1fa
Isak Friis-Jespersen <isak.friisjespersen2@gmail.com>
2025-12-07 16:26:15 +0000

    telescope args

--- nvim/lua/ifj/plugins/telescope.lua
+++ nvim/lua/ifj/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
   },
   config = function()
     local telescope = require("telescope")
@@ -29,6 +35,9 @@ return {
       --   },
       -- },
 
+      extension = {
+        fzf = {},
+      },
       defaults = {
         path_display = { "smart" },
         mappings = {
@@ -52,5 +61,7 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+    keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
   end,
 }
